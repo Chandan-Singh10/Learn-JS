f16d10e7de012e6a1a0d8ce4e391a687a2375b04
Chandan Singh <chandansinghcs9536@gmail.com>
2023-11-26 12:31:46 +0530

    config array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,40 @@
+// Array
+
+const myArray = [0, 1, 2, 3, 4, 5];
+// console.log(myArray[3]);
+
+// Array Methods
+
+// myArray.push(6);
+// myArray.push(7);
+// myArray.pop(); // last value of remove ker dega
+
+// myArray.unshift(9);
+// myArray.shift()
+
+// console.log(myArray.includes(20));
+// console.log(myArray.indexOf(30));
+
+const newArr = myArray.join(); // Data type ko objects, se string ker deta ha
+
+// console.log(myArray);
+// console.log(typeof myArray);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+
+// slice, splice
+console.log("A", myArray);
+
+const myn1 = myArray.slice(1, 3);
+
+console.log(myn1);
+console.log("B", myArray);
+
+const myn2 = myArray.splice(1, 3); // Original array ko menupulate Karta ha
+
+console.log("C", myArray);
+console.log(myn2);
+
+
+
