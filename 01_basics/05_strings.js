8496a7a3df38029c7b8c89ef044490127a2dc16e
Chandan Singh <chandansinghcs9536@gmail.com>
2023-11-26 12:32:23 +0530

    config

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "hitesh";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value"); // purana tarika nhi use karna 
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("hiteshhc");
+
+console.log(gameName[0]);
